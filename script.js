780a4004880fbe843a9d73058c48f2efa218819a
saumil8200 <saumil8200@gmail.com>
2024-01-14 10:30:42 +0530

    Added darkl-light mode as per system theme

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-const cursor = document.querySelector('.cursor');
-const colors = [
-  '#D8E9A8',
-  '#1E5128',
-  '#F7FEE7',
-  '#F0F0F0',
-  '#333333'
-];
-
-document.addEventListener('mousemove', (e) => {
-  cursor.style.left = e.pageX + 'px';
-  cursor.style.top = e.pageY + 'px';
-
-  const colorIndex = Math.floor(Math.random() * colors.length);
-  const randomColor = colors[colorIndex];
-  cursor.style.backgroundColor = randomColor;
-});
-
 document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll("nav ul a");
 
